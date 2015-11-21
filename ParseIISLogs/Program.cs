1bcfd0cdc1ca15d466a83b213fb65c2963eedac4
davidebbo <david.ebbo@microsoft.com>
2015-11-20 18:26:26 -0800

    Limit calculation to one month

--- ParseIISLogs/Program.cs
+++ ParseIISLogs/Program.cs
@@ -9,12 +9,25 @@ namespace ParseIISLogs
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("e.g. ParseIISLogs.exe 2015 08");
+                return;
+            }
+
+            string year = args[0];
+            string month = args[1];
+
             var users = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-            foreach (string file in Directory.GetFiles(".", "*.log", SearchOption.AllDirectories))
+
+            var allLogs = Directory.EnumerateFiles(".", "*.log", SearchOption.AllDirectories);
+
+            // e.g. .\EXPLORER-BAY\2015\07\03\02
+            foreach (string file in allLogs.Where(s => { var segments = s.Split('\\'); return segments[2] == year && segments[3] == month; }))
             {
                 foreach (string line in File.ReadAllLines(file))
                 {
-                    if (line[0] == '#') continue;
+                    if (!line.Contains('@')) continue;
 
                     var parts = line.Split(' ');
                     string user = parts[7];
@@ -24,9 +37,22 @@ namespace ParseIISLogs
             }
 
             Console.WriteLine($"Distinct users: {users.Count}");
-            foreach (string user in users.OrderBy(u => u))
+
+            //Console.WriteLine("***************************");
+
+            //foreach (string user in users.OrderBy(u => u))
+            //{
+            //    Console.WriteLine(user);
+            //}
+
+            Console.WriteLine("***************************");
+
+            var domains = from u in users
+                          group u by u.Split('@')[1] into g
+                          select new { Name = g.Key, Count = g.Count() };
+            foreach (var domain in domains.Where(d => d.Count > 5).OrderByDescending(d => d.Count))
             {
-                Console.WriteLine(user);
+                Console.WriteLine($"{domain.Name}: {domain.Count}");
             }
         }
     }
